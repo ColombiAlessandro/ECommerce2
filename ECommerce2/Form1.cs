051afae159a7bb600b9b069281fbe4258886993f
ColombiAlessandro <94958005+ColombiAlessandro@users.noreply.github.com>
2023-01-30 09:23:53 +0100

    Classe prodotto elettronico

---
+++ ECommerce2/Form1.cs
@@ -0,0 +1,10 @@
+namespace ECommerce2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ECommerce2/ProdottoElettronico.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce2
+{
+    public class ProdottoElettronico : Product
+    {
+        private string _modelloSpecifico;
+        public string ModelloSpecifico
+        {
+            get { return _modelloSpecifico; }
+            private set { _modelloSpecifico = value; }
+        }
+        public override Product Clone()
+        {
+            return new ProdottoElettronico(this);
+        }
+        public ProdottoElettronico(ProdottoElettronico prod) : base(prod)
+        {
+            ModelloSpecifico= prod.ModelloSpecifico;
+        }
+        public ProdottoElettronico(string modello, string id, string name, string prod, float price) : base(id,name,prod,price)
+        {
+            ModelloSpecifico = modello;
+        }
+    }
+}
