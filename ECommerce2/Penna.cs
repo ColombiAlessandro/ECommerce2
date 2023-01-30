f679b4626bb81964975c7476e829f212c0f31209
ColombiAlessandro <94958005+ColombiAlessandro@users.noreply.github.com>
2023-01-30 10:20:15 +0100

    classe penna

--- ECommerce2/Penna.cs
+++ ECommerce2/Penna.cs
@@ -8,5 +8,19 @@ namespace ECommerce2
 {
     public class Penna:ProdottoCancelleria
     {
+        private string _tipo;
+        public string Tipo
+        {
+            get { return _tipo; }
+            set { _tipo = value; }
+        }
+        public Penna(Penna p)
+        {
+            Tipo= p.Tipo;
+        }
+        public override Penna Clone()
+        {
+            return new Penna(this);
+        }
     }
 }
